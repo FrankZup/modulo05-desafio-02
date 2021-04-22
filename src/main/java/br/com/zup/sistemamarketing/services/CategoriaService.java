180c2d9e5a9e4b4e4e129590a4cea6723676e0d8
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-04-22 19:44:53 -0300

    Criar classe de serviço da Categoria

---
+++ src/main/java/br/com/zup/sistemamarketing/services/CategoriaService.java
@@ -0,0 +1,47 @@
+package br.com.zup.sistemamarketing.services;
+
+import br.com.zup.sistemamarketing.models.Categoria;
+import br.com.zup.sistemamarketing.repositories.CategoriaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+    private final CategoriaRepository categoriaRepository;
+
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public Categoria salvarCategoria(Categoria categoria){
+        return categoriaRepository.save(categoria);
+    }
+
+    public Iterable <Categoria> pesquisarTodasCategorias() {
+        return categoriaRepository.findAll();
+    }
+
+    public Categoria buscarCategoriaPeloId(int id){
+        Optional<Categoria> optionalCategoria = categoriaRepository.findById(id);
+
+        if( optionalCategoria.isPresent() ){
+            return optionalCategoria.get();
+        }
+
+        throw new CategoriaNaoExisteException();
+    }
+
+    public Categoria atualizarCategoria(Categoria categoriaAntiga){
+
+        Categoria categoriaAtual = buscarCategoriaPeloId(categoriaAntiga.getId());
+        categoriaAtual.setNome(categoriaAntiga.getNome());
+        return categoriaRepository.save(categoriaAntiga);
+    }
+
+    public void deletarCategoria(int id){
+        Categoria categoria = buscarCategoriaPeloId(id);
+
+        categoriaRepository.delete(categoria);
+    }
+}
