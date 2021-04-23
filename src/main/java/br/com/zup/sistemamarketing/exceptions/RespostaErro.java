c05b6ef01196ca0893c026e81b839751330e5659
Franklin Hanemann <franklin.hanemann@zup.com.br>
2021-04-22 21:04:47 -0300

    Criar classe Resposta de Erro

---
+++ src/main/java/br/com/zup/sistemamarketing/exceptions/RespostaErro.java
@@ -0,0 +1,30 @@
+package br.com.zup.sistemamarketing.exceptions;
+
+public class RespostaErro {
+    private String tipoErro;
+    private int codigo;
+    private String status;
+    private List<ObjetoErro> objetoErro;
+
+    public RespostaErro(String tipoErro, int codigo, String status, List<ObjetoErro> objetoErro) {
+        this.tipoErro = tipoErro;
+        this.codigo = codigo;
+        this.status = status;
+        this.objetoErro = objetoErro;
+    }
+
+    public RespostaErro() { }
+
+    public String getTipoErro() { return tipoErro; }
+    public void setTipoErro(String tipoErro) { this.tipoErro = tipoErro; }
+
+    public int getCodigo() { return codigo; }
+    public void setCodigo(int codigo) { this.codigo = codigo; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public List<ObjetoErro> getObjetoErro() { return objetoErro; }
+    public void setObjetoErro(List<ObjetoErro> objetoErro) { this.objetoErro = objetoErro; }
+
+}
